62c2f63a44cc6be37b1cbbe740c0ef345f9d1867
Alchenda <89601203+Alchenda@users.noreply.github.com>
2024-07-15 12:45:07 -0400

    LPT addition

---
+++ Autocorrelation_Noise_Removal.m
@@ -0,0 +1,70 @@
+%release all system memory
+clear all;
+
+%create basic sine wave. this will be used in place of the input sample
+%vector previously examined for CCR
+t = 0:pi / 100:2*4*pi;
+x = sin(t);
+
+%two sided correlation examined first
+N = 4;
+%create verticle vector of data
+y = [6:9]';
+%create the two sided correlation
+CCR2 = zeros(2 * N - 1, 1);
+
+%iterate over all possible lags from -N + 1 to N - 1
+for lag = -N + 1 : N - 1
+    %set the CC sum for each lag
+    CC = 0;
+    %loop over all of the signal indices, the lag is computed for the
+    %indices
+    for idx = 1 : N
+        lag_idx = idx - lag;
+        %ensure that the lag is within the bounds of our signal. If it is
+        %within bounds add to our CC
+        
+        %the only time this is true is when idx = 4, then lag_idx = 4 + 3 =
+        %1
+        if((lag_idx >= 1) && (lag_idx <= N))
+            CC = CC + x (idx) * y (lag_idx);
+        end
+    end
+    
+    %ensures correct indexing for postive and negative lags
+    CCR2(lag + N) = CC;
+end
+disp('Double sided CCR');
+disp(CCR2)
+
+%one-sided correlation
+CCR1 = zeros(N, 1);
+
+%iterate over all lags from = 0 to N-1
+for lag = 0 : N - 1
+    %set the CC sum for each lag
+    CC = 0;
+    %loop over all of the signal indices
+    for idx = lag + 1:N
+        lag_idx = idx - lag;
+        CC = CC + x(idx) * y(lag_idx);
+    end
+    
+    CCR1(lag + 1) = CC;
+end
+
+disp('Single sided CCR')
+disp(CCR1);
+
+%convulation can be used for calculating correlation
+
+%convolution reverses of the sequences at the start of its operation.
+%Correlation, we do not want this reversal. The 'flipud' function can be
+%used to re-reverse the y column, such that the convolution will generate
+%our cross-correlation
+CC = conv(x, flipud(y));
+
+disp('convolution based CCR');
+disp(CC);
+
+disp('Take time to observe that the CCR generated via the manual 2-sided CCR and Convolution CCR are the same');
